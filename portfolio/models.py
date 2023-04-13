2640ad7225137714e5006cda84ac51651f2f4547
axelbriandip <axelbriandip.rg@gmail.com>
2023-04-13 01:32:39 -0300

    add more info in projects

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name = 'Título')
     description = models.TextField(verbose_name = 'Descripción')
     img = models.ImageField(verbose_name = 'Imágen', upload_to='media/projects')
+    URLField = models.URLField(null = True, blank = True, verbose_name='Link')
     created = models.DateTimeField(auto_now_add=True, verbose_name = 'Creación') # solo se crea una vez
     updated = models.DateTimeField(auto_now=True, verbose_name = 'Última edición') # se va actualizando
 
